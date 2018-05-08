40cb53f08d8c6a77b49c769d3894146213f22d42
Srikanth Tummala <srikanth.soa@gmail.com>
2018-05-08 10:26:16 +0100

    Sample Spring Boot Project.

---
+++ src/main/java/srikanth/labs/springBoot2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package srikanth.labs.springBoot2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBoot2Application.class);
+	}
+
+}
